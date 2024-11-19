e64b75fc1e22c8630ac377852b63ae4ac1f0bb2a
wolfywitchybluees7 <wolfywitchybluees7@gmail.com>
2024-11-20 10:37:07 +1100

    yes

---
+++ src/main/java/net/wolfywitchybluees7/ggmod/GenerallyGeneralClient.java
@@ -0,0 +1,10 @@
+package net.wolfywitchybluees7.ggmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class GenerallyGeneralClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
