497eaaeeb2c183ebca07467fa302e1e6cccc321b
wolfywitchybluees7 <wolfywitchybluees7@gmail.com>
2024-11-20 10:38:21 +1100

    test change

--- src/main/java/net/wolfywitchybluees7/ggmod/GenerallyGeneral.java
+++ src/main/java/net/wolfywitchybluees7/ggmod/GenerallyGeneral.java
@@ -12,7 +12,9 @@ public class GenerallyGeneral implements ModInitializer {
 	// It is considered best practice to use your mod id as the logger's name.
 	// That way, it's clear which mod wrote info, warnings, and errors.
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
-	// some change
+
+	// new change
+
 	@Override
 	public void onInitialize() {
 		// This code runs as soon as Minecraft is in a mod-load-ready state.
